457f76c70047ef521a00d5f2902bcc6371877ccd
sammathur4 <60032069+sammathur4@users.noreply.github.com>
2021-01-07 21:01:03 +0530

    Update Text_Summarizer.py

--- Text_Summarizer.py
+++ Text_Summarizer.py
@@ -28,8 +28,10 @@ word_frequencies = {}
 for word in nltk.word_tokenize(formatted_article_text): 
     if word not in stopwords: 
         if word not in word_frequencies.keys(): 
-            word_frequencies[word] = 1        else: 
-            word_frequencies[word] += 1    maximum_frequncy = max(word_frequencies.values()) 
+            word_frequencies[word] = 1        
+      else: 
+            word_frequencies[word] += 1    
+      maximum_frequncy = max(word_frequencies.values()) 
  
 for word in word_frequencies.keys(): 
     word_frequencies[word] = (word_frequencies[word]/maximum_frequncy) 
